caa565fd34c0a6003ba046a96580269aa8a4e88b
glinchak3 <alex.vz1403@gmail.com>
2026-03-22 18:23:37 +0300

    Update TSP_1(3opt).cpp

--- TSP_1(3opt).cpp
+++ TSP_1(3opt).cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
-#include <algorithm>
 
 using namespace std;
 
@@ -14,14 +13,25 @@ double route_length(const vector<int>& route, const vector<double>& x, const vec
     double length = 0;
     int n = route.size();
     for (int i = 0; i < n; ++i) {
-        int next = (i + 1) % n;
-        length += dist(x[route[i]], y[route[i]], x[route[next]], y[route[next]]);
+        length += edge_dist(route, i, i + 1, x, y);
     }
     return length;
 }
 
-double part_dist(int a, int b, const vector<int>& route, const vector<double>& x, const vector<double>& y) {
-    return dist(x[route[a]], y[route[a]], x[route[b]], y[route[b]]);
+double edge_dist(const vector<int>& route, int i, int j, const vector<double>& x, const vector<double>& y) {
+    int n = route.size();
+    int a = route[(i + n) % n];
+    int b = route[(j + n) % n];
+    return dist(x[a], y[a], x[b], y[b]);
+}
+
+void reverse_part(vector<int>& route, int start, int end) {
+    int n = route.size();
+    while (start != end && (start + n - 1) % n != end) {
+        swap(route[start], route[end]);
+        start = (start + 1) % n;
+        end = (end - 1 + n) % n;
+    }
 }
 
 void three_opt(vector<int>& route, const vector<double>& x, const vector<double>& y) {
@@ -30,63 +40,50 @@ void three_opt(vector<int>& route, const vector<double>& x, const vector<double>
 
     while (improved) {
         improved = false;
-        bool loc = false;
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 2; j < n; ++j) {
+                for (int k = j + 2; k < n + (i > 0 ? 1 : 0); ++k) {
+                    int a = i % n;
+                    int b = (i + 1) % n;
+                    int c = j % n;
+                    int d = (j + 1) % n;
+                    int e = k % n;
+                    int f = (k + 1) % n;
 
-        for (int i = 0; i < n - 5 && !loc; ++i) {
-            for (int j = i + 2; j < n - 3 && !loc; ++j) {
-                for (int k = j + 2; k < n - (i == 0 ? 1 : 0) && !loc; ++k) {
-                    int a = i, b = i + 1;
-                    int c = j, d = j + 1;
-                    int e = k, f = (k + 1) % n;
+                    double old_dist = edge_dist(route, a, b, x, y) + edge_dist(route, c, d, x, y) + edge_dist(route, e, f, x, y);
 
-                    double old_dist = dist(x[route[a]], y[route[a]], x[route[b]], y[route[b]]) + dist(x[route[c]], y[route[c]], x[route[d]], y[route[d]]) + dist(x[route[e]], y[route[e]], x[route[f]], y[route[f]]);
+                    double new_dist;
                     vector<int> tmp = route;
-                    reverse(tmp.begin() + b, tmp.begin() + c + 1);
-                    double new_dist = dist(x[tmp[a]], y[tmp[a]], x[tmp[c]], y[tmp[c]]) + dist(x[tmp[b]], y[tmp[b]], x[tmp[d]], y[tmp[d]]) + dist(x[tmp[e]], y[tmp[e]], x[tmp[f]], y[tmp[f]]);
+                    reverse_part(tmp, b, c);
+                    new_dist = edge_dist(tmp, a, b, x, y) + edge_dist(tmp, c, d, x, y) + edge_dist(tmp, e, f, x, y);
                     if (new_dist < old_dist) {
                         route = tmp;
                         improved = true;
-                        loc = true;
-                        continue;
-                    }
-                    tmp = route;
-                    reverse(tmp.begin() + d, tmp.begin() + e + 1);
-                    new_dist = dist(x[tmp[a]], y[tmp[a]], x[tmp[b]], y[tmp[b]]) + dist(x[tmp[c]], y[tmp[c]], x[tmp[e]], y[tmp[e]]) + dist(x[tmp[d]], y[tmp[d]], x[tmp[f]], y[tmp[f]]);
-                    if (new_dist < old_dist) {
-                        route = tmp;
-                        improved = true;
-                        loc = true;
-                        continue;
+                        break;
                     }
 
                     tmp = route;
-                    reverse(tmp.begin() + b, tmp.begin() + c + 1);
-                    reverse(tmp.begin() + d, tmp.begin() + e + 1);
-                    new_dist = dist(x[tmp[a]], y[tmp[a]], x[tmp[c]], y[tmp[c]]) + dist(x[tmp[b]], y[tmp[b]], x[tmp[e]], y[tmp[e]]) + dist(x[tmp[d]], y[tmp[d]], x[tmp[f]], y[tmp[f]]);
+                    reverse_part(tmp, d, e);
+                    new_dist = edge_dist(tmp, a, b, x, y) + edge_dist(tmp, c, d, x, y) + edge_dist(tmp, e, f, x, y);
                     if (new_dist < old_dist) {
                         route = tmp;
                         improved = true;
-                        loc = true;
-                        continue;
+                        break;
                     }
 
                     tmp = route;
-                    vector<int> part1(route.begin() + b, route.begin() + d + 1);
-                    vector<int> part2(route.begin() + d + 1, route.begin() + e + 1);
-
-                    tmp.erase(tmp.begin() + b, tmp.begin() + e + 1);
-                    tmp.insert(tmp.begin() + b, part2.begin(), part2.end());
-                    tmp.insert(tmp.begin() + b + part2.size(), part1.begin(), part1.end());
-
-                    new_dist = dist(x[tmp[a]], y[tmp[a]], x[tmp[b]], y[tmp[b]]) + dist(x[tmp[b + (int)part2.size() - 1]], y[tmp[b + (int)part2.size() - 1]], x[tmp[b + (int)part2.size()]], y[tmp[b + (int)part2.size()]]) + dist(x[tmp[e]], y[tmp[e]], x[tmp[f]], y[tmp[f]]);
+                    reverse_part(tmp, b, c);
+                    reverse_part(tmp, d, e);
+                    new_dist = edge_dist(tmp, a, b, x, y) + edge_dist(tmp, c, d, x, y) + edge_dist(tmp, e, f, x, y);
                     if (new_dist < old_dist) {
                         route = tmp;
                         improved = true;
-                        loc = true;
-                        continue;
+                        break;
                     }
                 }
+                if (improved) break;
             }
+            if (improved) break;
         }
     }
 }
